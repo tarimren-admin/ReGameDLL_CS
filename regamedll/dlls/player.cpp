2a01a45cdb0ae6cdd22309613729c1122b14bc5b
tarimren <60697225+tarimren-admin@users.noreply.github.com>
2025-04-11 11:09:42 +0800

    Update player.cpp

--- regamedll/dlls/player.cpp
+++ regamedll/dlls/player.cpp
@@ -5661,11 +5661,20 @@ void CBasePlayer::SetScoreAttrib(CBasePlayer *dest)
 #endif
 
 #ifdef REGAMEDLL_FIXES
+	if (
+     #ifdef REGAMEDLL_ADD
+     show_c4_defkit.value
+#else
+	 false
+#endif
+	  )
+	{
 	// TODO: Remove these fixes when they are implemented on the client side
 	if (state & (SCORE_STATUS_BOMB | SCORE_STATUS_DEFKIT) && GetForceCamera(dest) != CAMERA_MODE_SPEC_ANYONE)
-	{
+	  {
 		if (CSGameRules()->PlayerRelationship(this, dest) != GR_TEAMMATE)
-			state &= ~(SCORE_STATUS_BOMB | SCORE_STATUS_DEFKIT);
+	    	state &= ~(SCORE_STATUS_BOMB | SCORE_STATUS_DEFKIT);
+	  }
 	}
 #endif
 
