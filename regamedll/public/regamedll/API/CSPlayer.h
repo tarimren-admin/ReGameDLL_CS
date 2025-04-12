bdb10edd63b16d324c26f432721ef0cf84bc9f69
tarimren <60697225+tarimren-admin@users.noreply.github.com>
2025-04-12 22:34:10 +0800

    Update CSPlayer.h

--- regamedll/public/regamedll/API/CSPlayer.h
+++ regamedll/public/regamedll/API/CSPlayer.h
@@ -93,7 +93,6 @@ public:
 	virtual void SendItemStatus();
 	virtual void ReloadWeapons(CBasePlayerItem *pWeapon = nullptr, bool bForceReload = false, bool bForceRefill = false);
 	virtual void Observer_SetMode(int iMode);
-	virtual void Observer_FindNextPlayer(bool bReverse, const char *name = nullptr);
 	virtual bool SelectSpawnSpot(const char *pEntClassName, CBaseEntity* &pSpot);
 	virtual bool SwitchWeapon(CBasePlayerItem *pWeapon);
 	virtual void SwitchTeam();
